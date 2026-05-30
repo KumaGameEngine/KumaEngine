73928e1b816856bfacb4fcac18dbd39e50446a94
Samma <81291107+Samma2009@users.noreply.github.com>
2026-05-30 16:13:36 +0200

    suppress glsl extension errors

--- RocketEngine/Shaders/skybox/fragment.glsl
+++ RocketEngine/Shaders/skybox/fragment.glsl
@@ -5,8 +5,8 @@ layout(set = 0, binding = 0) uniform ProjView
     mat4 View;
     mat4 Proj;
 };
-layout(set = 1, binding = 0) uniform textureCube SkyboxTex;
-layout(set = 1, binding = 1) uniform sampler SkyboxSamp;
+layout(set = 1, binding = 0) uniform textureCube SkyboxTex; //!
+layout(set = 1, binding = 1) uniform sampler SkyboxSamp;    //!
 
 layout(location = 0) in vec4 fragDir;
 layout(location = 0) out vec4 outputColor;
@@ -18,7 +18,7 @@ void main()
 
     vec3 rayDir = normalize((inverse(View) * vec4(viewPos.xyz, 0.0)).xyz);
 
-	outputColor = vec4(texture(samplerCube(SkyboxTex, SkyboxSamp), rayDir).xyz,1);
+	outputColor = vec4(texture(samplerCube(SkyboxTex, SkyboxSamp), rayDir).xyz,1); //!
 
     gl_FragDepth = 1.0;
 }
--- RocketEngine/Shaders/ui/fragment.glsl
+++ RocketEngine/Shaders/ui/fragment.glsl
@@ -1,7 +1,7 @@
 #version 450
 
-layout(set = 1, binding = 0) uniform texture2D ImageTex;
-layout(set = 1, binding = 1) uniform sampler ImageSamp;
+layout(set = 1, binding = 0) uniform texture2D ImageTex; //!
+layout(set = 1, binding = 1) uniform sampler ImageSamp;  //!
 
 layout(location = 0) in vec2 fUV;
 layout(location = 1) in vec4 fCol;
@@ -9,7 +9,7 @@ layout(location = 0) out vec4 outputColor;
 
 void main()
 {
-	vec4 col = texture(sampler2D(ImageTex, ImageSamp), fUV);
+	vec4 col = texture(sampler2D(ImageTex, ImageSamp), fUV); //! vec4 col;
 
 	outputColor = col * fCol;
 
