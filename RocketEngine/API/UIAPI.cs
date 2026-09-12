45a8818b9dfc3faf0bedc4b39c0fb81dfaed6e82
Samma <terminal.asm@gmail.com>
2026-09-12 18:07:35 +0200

    Update UIAPI.cs

--- RocketEngine/API/UIAPI.cs
+++ RocketEngine/API/UIAPI.cs
@@ -33,9 +33,9 @@ namespace KumaEngine.API
             })
         ];
 
-        public static void PushElement(this Lua lua,int handle, string id)
+        public static void PushElement(this Lua lua, Element element)
         {
-            var MODNAME = "element." + handle;
+            var MODNAME = "element." + element.NativePtr;
 
             lua.NewTable();
 
@@ -45,10 +45,165 @@ namespace KumaEngine.API
                 string p = L.ToString(1);
                 string v = L.ToString(2);
 
-                UISurfaceHandles[handle].GetElementById(id)!.SetAttribute(p, v);
+                element.SetAttribute(p, v);
 
                 return 0;
             });
+
+            lua.PushSafeCFunction("setProperty", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+                string v = L.ToString(2);
+
+                element.SetProperty(p, v);
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("addClass", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                element.AddClass(p);
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("removeClass", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                element.RemoveClass(p);
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("removeAttribute", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                element.RemoveAttribute(p);
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("getAttribute", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                L.PushString(element.GetAttribute(p));
+
+                return 1;
+            });
+
+            lua.PushSafeCFunction("getProperty", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                L.PushString(element.GetProperty(p));
+
+                return 1;
+            });
+
+            lua.PushSafeCFunction("hasAttribute", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                L.PushBoolean(element.HasAttribute(p));
+
+                return 1;
+            });
+
+            lua.PushSafeCFunction("hasClass", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                L.PushBoolean(element.HasClass(p));
+
+                return 1;
+            });
+
+            lua.PushSafeCFunction("click", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                element.Click();
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("focus", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                element.Focus();
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("blur", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+
+                element.Blur();
+
+                return 0;
+            });
+
+            lua.PushSafeCFunction("addEventListener", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string p = L.ToString(1);
+                var lfunc = L.GetLuaFunctionArgRef(2);
+
+                element.AddEventListener(p,ev =>
+                {
+                    L.PushElement(ev.TargetElement is null ? ev.CurrentElement! : ev.TargetElement);
+                    L.CallLuaFunction(lfunc,1);
+                });
+
+                return 0;
+            });
+
+            lua.NewTable();
+
+            lua.PushSafeCFunction("__index", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string key = L.ToString(2);
+
+                switch (key)
+                {
+                    case "innerRml": L.PushString(element.GetInnerRml()); return 1;
+                    default: L.PushNil(); return 1;
+                }
+            });
+
+            lua.PushSafeCFunction("__newindex", MODNAME, ilua =>
+            {
+                var L = Lua.FromIntPtr(ilua);
+                string key = L.ToString(2);
+
+                switch (key)
+                {
+                    case "innerRml": element.SetInnerRml(L.ToString(3)); break;
+                }
+
+                return 0;
+            });
+
+            lua.SetMetaTable(-2);
         }
 
         public static void PushUISurface(this Lua lua, UISurface surface)
@@ -90,7 +245,7 @@ namespace KumaEngine.API
                 var L = Lua.FromIntPtr(ilua);
                 var id = lua.ToString(1);
 
-                lua.PushElement(handle, id);
+                lua.PushElement(UISurfaceHandles[handle].GetElementById(id)!);
                 return 1;
             });
 
